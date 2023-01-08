26791293a5bd883e62535922fb60fe3d9d892d6e
sidorsett <49516540+sidorsett@users.noreply.github.com>
2023-01-08 17:01:05 +0100

    Update Form1.Designer.cs

--- BrazosTweaker/Form1.Designer.cs
+++ BrazosTweaker/Form1.Designer.cs
@@ -543,7 +543,7 @@
             this.MaximizeBox = false;
             this.MinimumSize = new System.Drawing.Size(380, 210);
             this.Name = "Form1";
-            this.Text = "BrazosTweaker V1.1.1";
+            this.Text = "BrazosTweaker v1.1.1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.tabControl1.ResumeLayout(false);
             this.tabPageP0.ResumeLayout(false);
